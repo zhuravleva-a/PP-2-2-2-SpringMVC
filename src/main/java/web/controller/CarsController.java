2c60b4398c2695bacef3d805413145923818cf0e
Anastasia Zhuravleva <agzhuravleva@gmail.com>
2022-01-22 12:44:51 +0300

    Update: Change HttpServletRequest to @RequestParam

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -3,6 +3,7 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
 import web.service.CarService;
 import web.service.CarServiceImpl;
@@ -13,11 +14,11 @@ import javax.servlet.http.HttpServletRequest;
 public class CarsController {
 
     @GetMapping(value = "/cars")
-    public String printCar(ModelMap model, HttpServletRequest request) {
+    public String printCar(@RequestParam(value = "count", required = false) String count, ModelMap model) {
         CarService carService = new CarServiceImpl();
-        if (request.getParameter("count") != null) {
-            model.addAttribute("cars", carService.getCars(Integer.parseInt(request.getParameter("count"))));
-        } else {
+        try {
+            model.addAttribute("cars", carService.getCars(Integer.parseInt(count)));
+        } catch (NumberFormatException e) {
             model.addAttribute("cars", carService.getCars(5));
         }
 
